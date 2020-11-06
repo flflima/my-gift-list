302a7289873e65f6b0efb865cc1e283fdeecc855
Felipe Lima Freire <felipe.lima.freire@concrete.com.br>
2020-11-06 10:49:54 -0300

    Remove testes para build inicial

--- src/test/kotlin/br/com/dev/mygiftlist/MyGiftListApplicationTests.kt
+++ src/test/kotlin/br/com/dev/mygiftlist/MyGiftListApplicationTests.kt
@@ -3,11 +3,11 @@ package br.com.dev.mygiftlist
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class MyGiftListApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class MyGiftListApplicationTests {
+//
+//	@Test
+//	fun contextLoads() {
+//	}
+//
+//}
